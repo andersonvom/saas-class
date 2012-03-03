dc38b38914b226c1a8eb1dc31c4bcab7bbf2c7a7
Anderson Mesquita <andersonvom@gmail.com>
2012-03-03 18:22:40 -0500

    RPS: Tournament passed all tests

--- assignments/01.ruby.calisthenics/lib/02_rock_paper_scissors.rb
+++ assignments/01.ruby.calisthenics/lib/02_rock_paper_scissors.rb
@@ -12,3 +12,23 @@ def rps_game_winner(game)
   game[0][1] == STRATEGIES[game[1][1]] ? game[1] : game[0]
 end
 
+
+# Returns true if an array if a player. False otherwise.
+def rps_is_player?(arr)
+  arr[0].kind_of? String
+end
+
+
+# Takes a tournament encoded as a bracketed array and returns the winner
+# The array has to be well formed (that is, there are 2^n players,
+# and each one participates in exactly one match per round).
+# That is, a tournament is a game where each player is either a player
+# or another tournament.
+def rps_tournament_winner(tournament)
+  return tournament if rps_is_player? tournament
+
+  player_one = rps_tournament_winner(tournament[0])
+  player_two = rps_tournament_winner(tournament[1])
+
+  rps_game_winner([player_one, player_two])
+end
