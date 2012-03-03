fb92b39c780253049f1f43486a5601483166adfc
Anderson Mesquita <andersonvom@gmail.com>
2012-03-03 14:46:45 -0500

    Added documentation to palindrome method

--- assignments/01.ruby.calisthenics/lib/01_fun_with_strings.rb
+++ assignments/01.ruby.calisthenics/lib/01_fun_with_strings.rb
@@ -1,6 +1,11 @@
 
+# Determines whether a given word or phrase is a palindrome, that is,
+# it reads the same backwards as forwards, ignoring case, punctuation, and
+# nonword characters. (A "nonword character" is defined for our purposes as "a
+# character that Ruby regexps would treat as a nonword character".)
 def palindrome?(string)
   test_string = string.gsub(/[^\w]/,'').downcase
   test_string == test_string.reverse
 end
+
 
