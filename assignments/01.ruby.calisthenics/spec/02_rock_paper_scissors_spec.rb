259aaba5d3051d84f27a28e5d7cca1b0ed4f9e0d
Anderson Mesquita <andersonvom@gmail.com>
2012-03-03 17:17:49 -0500

    RPS: added tournament test

--- assignments/01.ruby.calisthenics/spec/02_rock_paper_scissors_spec.rb
+++ assignments/01.ruby.calisthenics/spec/02_rock_paper_scissors_spec.rb
@@ -1,7 +1,7 @@
 require '02_rock_paper_scissors'
 
 
-describe Object do
+describe "RPS" do
 
   before :each do
     @hands = [
@@ -15,29 +15,52 @@ describe Object do
       ["Richard X.", "P"],
     ]
     @invalid_hand = ["Invalid", "X"]
+
+    @tournament = [
+      [
+        [@hands[0], @hands[1]],
+        [@hands[2], @hands[3]],
+      ],
+      [
+        [@hands[4], @hands[5]],
+        [@hands[6], @hands[7]],
+      ],
+    ]
   end
 
-  it "should raise error if number of players is not 2" do
-    lambda {rps_game_winner([@hands[0]])}.should raise_error WrongNumberOfPlayersError
-    lambda {rps_game_winner([@hands[0], @hands[1]])}.should_not raise_error WrongNumberOfPlayersError
-    lambda {rps_game_winner([@hands[0], @hands[1], @hands[2]])}.should raise_error WrongNumberOfPlayersError
+  describe "During game:" do
+
+    it "should raise error if number of players is not 2" do
+      lambda {rps_game_winner([@hands[0]])}.should raise_error WrongNumberOfPlayersError
+      lambda {rps_game_winner([@hands[0], @hands[1]])}.should_not raise_error WrongNumberOfPlayersError
+      lambda {rps_game_winner([@hands[0], @hands[1], @hands[2]])}.should raise_error WrongNumberOfPlayersError
+    end
+
+    it "should raise error if player if not going with Rock, Paper or Scissors" do
+      lambda {rps_game_winner([@hands[0], @invalid_hand])}.should raise_error NoSuchStrategyError
+      lambda {rps_game_winner([@invalid_hand, @hands[0]])}.should raise_error NoSuchStrategyError
+      lambda {rps_game_winner([@hands[0], @hands[1]])}.should_not raise_error NoSuchStrategyError
+    end
+
+    it "should let the first player win if they go with same strategy" do
+      rps_game_winner([@hands[0], @hands[5]]).should == @hands[0]
+      rps_game_winner([@hands[5], @hands[0]]).should == @hands[5]
+    end
+
+    it "should return the winning player and strategy" do
+      rps_game_winner([@hands[0], @hands[1]]).should == @hands[1]
+      rps_game_winner([@hands[0], @hands[2]]).should == @hands[0]
+      rps_game_winner([@hands[1], @hands[2]]).should == @hands[2]
+    end
+
   end
 
-  it "should raise error if player if not going with Rock, Paper or Scissors" do
-    lambda {rps_game_winner([@hands[0], @invalid_hand])}.should raise_error NoSuchStrategyError
-    lambda {rps_game_winner([@invalid_hand, @hands[0]])}.should raise_error NoSuchStrategyError
-    lambda {rps_game_winner([@hands[0], @hands[1]])}.should_not raise_error NoSuchStrategyError
-  end
+  describe "During Tournament:" do
 
-  it "should let the first player win if they go with same strategy" do
-    rps_game_winner([@hands[0], @hands[5]]).should == @hands[0]
-    rps_game_winner([@hands[5], @hands[0]]).should == @hands[5]
-  end
+    it "should play all games and return one single winner" do
+      rps_tournament_winner(@tournament).should == @hands[2]
+    end
 
-  it "should return the winning player and strategy" do
-    rps_game_winner([@hands[0], @hands[1]]).should == @hands[1]
-    rps_game_winner([@hands[0], @hands[2]]).should == @hands[0]
-    rps_game_winner([@hands[1], @hands[2]]).should == @hands[2]
   end
 
 end
