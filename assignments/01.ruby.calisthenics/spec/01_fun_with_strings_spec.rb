31d04f09a498ffab8e7183f2df1bbf3245f1a536
Anderson Mesquita <andersonvom@gmail.com>
2012-03-03 14:39:10 -0500

    Added palindrome tests

--- assignments/01.ruby.calisthenics/spec/01_fun_with_strings_spec.rb
+++ assignments/01.ruby.calisthenics/spec/01_fun_with_strings_spec.rb
@@ -2,7 +2,11 @@ require '01_fun_with_strings'
 
 describe Object do
 
-  it "should recognize a palindrome"
+  it "should recognize a palindrome" do
+    palindrome?("A man, a plan, a canal -- Panama").should be_true
+    palindrome?("Madam, I'm Adam!").should be_true
+    palindrome?("Abracadabra").should be_false
+  end
 
   it "should count words in a string"
 
