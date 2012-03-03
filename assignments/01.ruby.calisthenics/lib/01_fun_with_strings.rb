0f8175b927083d8eefac3fd379ce3a23e125ac6a
Anderson Mesquita <andersonvom@gmail.com>
2012-03-03 14:27:59 -0500

    Added SaaS first exercise (Ruby Calisthenics) skeleton

---
+++ assignments/01.ruby.calisthenics/lib/01_fun_with_strings.rb
@@ -0,0 +1,3 @@
+def palindrome?(sentence)
+end
+
---
+++ assignments/01.ruby.calisthenics/spec/01_fun_with_strings_spec.rb
@@ -0,0 +1,9 @@
+require '01_fun_with_strings'
+
+describe Object do
+
+  it "should recognize a palindrome"
+
+  it "should count words in a string"
+
+end
