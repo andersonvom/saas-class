6a95842bc2c077b07644e0875d874a85552b1ca5
Anderson Mesquita <andersonvom@gmail.com>
2012-03-07 20:56:03 -0500

    Open Classes: Fixed floating point precision of specs

--- assignments/02.more.ruby.and.some.rails/spec/01_open_classes_spec.rb
+++ assignments/02.more.ruby.and.some.rails/spec/01_open_classes_spec.rb
@@ -1,40 +1,41 @@
 require '01_open_classes'
 
-describe "open classes, metaprogramming, duck typing" do
+describe "Open Classes" do
 
   before :each do
+    @precision = 0.001
     @currencies = %w(dollar euro rupee yen)
   end
 
   it "should convert to euros (1.292)" do
-    6.euros.should == 7.752
+    6.euros.should be_within(@precision).of 7.752
   end
 
   it "should convert to rupees (0.019)" do
-    7.rupees.should == 0.133
+    7.rupees.should be_within(@precision).of 0.133
   end
 
   it "should convert to yen (0.013)" do
-    8.yens.should == 0.104
+    8.yens.should be_within(@precision).of 0.104
   end
 
   it "should convert to dollars (1)" do
-    9.75.dollars.should == 9.75
+    9.75.dollars.should be_within(@precision).of 9.75
   end
 
   it "should accept singular and plural forms of conversion" do
     @currencies.each do |singular_currency|
       plural_currency = "#{singular_currency}s"
+      5.send(plural_currency).should   be_a Numeric
       5.send(singular_currency).should be_a Numeric
-      5.send(plural_currency).should be_a Numeric
     end
   end
 
   it "should convert currency into another one" do
-    7.dollars.in(:euros).should == 5.418
-    7.euros.in(:rupees).should == 476
-    7.rupees.in(:yens).should == 10.231
-    7.yens.in(:dollars).should == 0.091
+    7.dollars.in(:euros).should be_within(@precision).of   5.418
+    7.euros.in(:rupees).should  be_within(@precision).of 476.000
+    7.rupees.in(:yens).should   be_within(@precision).of  10.231
+    7.yens.in(:dollars).should  be_within(@precision).of   0.091
   end
 
   it "should accept singular and plural symbols" do
