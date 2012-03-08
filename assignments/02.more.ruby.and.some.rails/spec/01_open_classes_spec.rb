df1c55e72ac3fdb0b316ed4bbdc3e9c0141d3e43
Anderson Mesquita <andersonvom@gmail.com>
2012-03-07 20:18:08 -0500

    Open Classes: added test skeleton and starting code sample

---
+++ assignments/02.more.ruby.and.some.rails/spec/01_open_classes_spec.rb
@@ -0,0 +1,16 @@
+require '01_open_classes'
+
+describe "open classes, metaprogramming, duck typing" do
+
+  it "should convert to dollars (1)"
+
+  it "should convert to euros (1.292)"
+
+  it "should convert to rupees (0.019)"
+
+  it "should convert to yen (0.013)"
+
+  it "should accept plural forms of conversion"
+
+end
+
