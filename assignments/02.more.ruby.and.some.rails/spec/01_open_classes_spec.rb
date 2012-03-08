3ed4b39747ce3d89c548ce5ec52da2b48af4ae8e
Anderson Mesquita <andersonvom@gmail.com>
2012-03-07 20:47:15 -0500

    Open Classes: Added spec tests

--- assignments/02.more.ruby.and.some.rails/spec/01_open_classes_spec.rb
+++ assignments/02.more.ruby.and.some.rails/spec/01_open_classes_spec.rb
@@ -2,15 +2,48 @@ require '01_open_classes'
 
 describe "open classes, metaprogramming, duck typing" do
 
-  it "should convert to dollars (1)"
+  before :each do
+    @currencies = %w(dollar euro rupee yen)
+  end
 
-  it "should convert to euros (1.292)"
+  it "should convert to euros (1.292)" do
+    6.euros.should == 7.752
+  end
 
-  it "should convert to rupees (0.019)"
+  it "should convert to rupees (0.019)" do
+    7.rupees.should == 0.133
+  end
 
-  it "should convert to yen (0.013)"
+  it "should convert to yen (0.013)" do
+    8.yens.should == 0.104
+  end
 
-  it "should accept plural forms of conversion"
+  it "should convert to dollars (1)" do
+    9.75.dollars.should == 9.75
+  end
+
+  it "should accept singular and plural forms of conversion" do
+    @currencies.each do |singular_currency|
+      plural_currency = "#{singular_currency}s"
+      5.send(singular_currency).should be_a Numeric
+      5.send(plural_currency).should be_a Numeric
+    end
+  end
+
+  it "should convert currency into another one" do
+    7.dollars.in(:euros).should == 5.418
+    7.euros.in(:rupees).should == 476
+    7.rupees.in(:yens).should == 10.231
+    7.yens.in(:dollars).should == 0.091
+  end
+
+  it "should accept singular and plural symbols" do
+    @currencies.each do |singular_currency|
+      plural_currency = "#{singular_currency}s"
+      7.dollars.in(singular_currency.to_sym).should be_a Numeric
+      7.dollars.in(plural_currency.to_sym).should be_a Numeric
+    end
+  end
 
 end
 
